38c68619a172f734c840be1aba52f1199eef4853
vitalzero <vitalzero@gmail.com>
2020-02-06 17:19:53 -0600

    Se agregó sprite de mushroom y rebota en las paredes (update)

--- SFML-Tut/Main.cpp
+++ SFML-Tut/Main.cpp
@@ -4,13 +4,19 @@ int main()
 {
 	sf::RenderWindow window( sf::VideoMode( 640, 480 ), "SFML tut!" );
 
+	sf::Texture texture;
+	if ( !texture.loadFromFile( "img/Mushroom.png" ) )
+	{
+		throw("Error loading img Mushroom.png");
+	}
+
+	sf::Sprite sprite( texture );
+	sf::Vector2u size = texture.getSize();
+	sprite.setOrigin( (float)size.x / 2, (float)size.y / 2 );
+	sf::Vector2f increment( 1.8f, 1.8f );
+
 	while ( window.isOpen() )
 	{
-		sf::RectangleShape rectangle( sf::Vector2f( 128.0f, 128.0f ) );
-		rectangle.setPosition( 320, 240 );
-		rectangle.setFillColor( sf::Color::Red );
-		rectangle.setOrigin( rectangle.getSize().x / 2, rectangle.getSize().y / 2 );
-
 		sf::Event event;
 		while ( window.pollEvent( event ) )
 		{
@@ -19,10 +25,23 @@ int main()
 				window.close();
 			}
 		}
+		// update
+		if ( (sprite.getPosition().x + (size.x / 2) >= window.getSize().x && increment.x > 0) ||
+			(sprite.getPosition().x - (size.x / 2) < 0 && increment.x < 0) )
+		{
+			increment.x = -increment.x;
+		}
+		if ( (sprite.getPosition().y + (size.y / 2) >= window.getSize().y && increment.y > 0) ||
+			(sprite.getPosition().y - (size.y / 2) < 0 && increment.y < 0) )
+		{
+			increment.y = -increment.y;
+		}
+		sprite.setPosition( sprite.getPosition() + increment );
+		//end update
 
-		window.clear( sf::Color::Black );
+		window.clear( sf::Color::White );
 		// init draw
-		window.draw( rectangle );
+		window.draw( sprite );
 		// end draw
 		window.display();
 	}
