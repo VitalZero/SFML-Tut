d69d7a029747fdbe9fc3e1379fc53107fd81a536
vitalzero <vitalzero@gmail.com>
2020-02-07 16:29:59 -0600

    Contador fps

--- SFML-Tut/Main.cpp
+++ SFML-Tut/Main.cpp
@@ -1,9 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include "Game.h"
+#include <iostream>
 
 int main()
 {
 	Game game;
+	
+	float lastTime = 0.f; // fps
 
 	while ( !game.GetWindow()->IsDone() )
 	{
@@ -11,6 +14,11 @@ int main()
 		game.Update();
 		game.Render();
 		game.RestartClock();
+		float currentTime = game.GetElapsed().asSeconds(); // fps
+		float fps = 1.0f / currentTime; // fps
+		lastTime = currentTime; // fps
+
+		//std::cout << "FPS: " << fps << "\n"; // fps
 	}
 
 	return 0;
