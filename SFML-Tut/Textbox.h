f091e8b774d56b05bda843deee3e8e4f018068cb
Jonathan Michel <vitalzero@gmail.com>
2020-02-14 00:01:54 -0600

    Se agrega clase text box

---
+++ SFML-Tut/Textbox.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+using MessageContainer = std::vector<std::string>;
+
+class Textbox
+{
+public:
+	Textbox();
+	Textbox( int visible, int charSize, int width, sf::Vector2f screenPos );
+	~Textbox();
+
+public:
+	void Setup( int visible, int charSize, int width, sf::Vector2f screenPos );
+	void Add( std::string& message );
+	void Clear();
+	void Render( sf::RenderWindow& window );
+
+private:
+	MessageContainer messages;
+	int numVisible;
+	sf::RectangleShape backdrop;
+	sf::Font font;
+	sf::Text content;
+};
