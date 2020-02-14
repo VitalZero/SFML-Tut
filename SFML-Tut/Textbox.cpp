23b72c2015dbc6dd329d1674874484d746579316
Jonathan Michel <vitalzero@gmail.com>
2020-02-14 00:19:06 -0600

    clase textbox terminada, se agregó la fuente arial

---
+++ SFML-Tut/Textbox.cpp
@@ -0,0 +1,67 @@
+#include "Textbox.h"
+#include <algorithm>
+
+Textbox::Textbox()
+{
+	Setup( 5, 9, 200, sf::Vector2f( 0, 0 ) );
+}
+
+Textbox::Textbox( int visible, int charSize, int width, sf::Vector2f screenPos )
+{
+	Setup( visible, charSize, width, screenPos );
+}
+
+Textbox::~Textbox()
+{
+	Clear();
+}
+
+void Textbox::Setup( int visible, int charSize, int width, sf::Vector2f screenPos )
+{
+	numVisible = visible;
+	sf::Vector2f offset( 2.0f, 20.f );
+
+	font.loadFromFile( "fonts/arial.ttf" );
+	content.setFont( font );
+	content.setString( "" );
+	content.setCharacterSize( charSize );
+	content.setColor( sf::Color::White );
+	content.setPosition( screenPos + offset);
+
+	backdrop.setSize( sf::Vector2f( width, (visible* charSize * 1.2f) ) );
+	backdrop.setFillColor( sf::Color( 90, 90, 90, 90 ) );
+	backdrop.setPosition( screenPos );
+}
+
+void Textbox::Add( std::string & message )
+{
+	messages.push_back( message );
+
+	if ( messages.size() >= 6 )
+	{
+		std::swap( *messages.begin(), *(messages.end() - 1) );
+		messages.pop_back();
+	}
+}
+
+void Textbox::Clear()
+{
+	messages.clear();
+}
+
+void Textbox::Render( sf::RenderWindow & window )
+{
+	std::string tmpContent;
+
+	for ( auto& itr : messages )
+	{
+		tmpContent.append( itr + "\n" );
+	}
+
+	if ( tmpContent != "" )
+	{
+		content.setString( tmpContent );
+		window.draw( backdrop );
+		window.draw( content );
+	}
+}
